dc8d4738733a3d629b265dc6fb08a47c3c497bc0
anuragv668 <anuragvarma668@gmail.com>
2023-10-03 11:22:14 +0530

    complete 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+  if (num < 0) {
+    return 'ERROR';
+  }
+  let muliplied = '';
+  for (let i = 0; i < num; i++) {
+    muliplied += string;
+  }
+  return muliplied;
 };
 
 // Do not edit below this line
