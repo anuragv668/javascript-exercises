24df94885cef877b94163da9a8287d61c2b1eed3
anuragv668 <anuragvarma668@gmail.com>
2023-10-03 12:27:15 +0530

    complete 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let rev = '';
+  for (let i = 0; i < string.length; i++) {
+    rev += string[string.length - 1 - i];
+  }
+  return rev;
 };
 
 // Do not edit below this line
