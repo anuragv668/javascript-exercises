f0474c2a334e0a5615ffe038c0f598993f4cbd38
anuragv668 <anuragvarma668@gmail.com>
2023-10-04 13:19:22 +0530

    complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...remove) {
+  for (let i = 0; i < remove.length; i++) {
+  let index = array.indexOf(remove[i]);
+  if (index == -1) {
+    continue;
+  }
+  array.splice(index, 1);
+  }
+  return array;
 };
 
 // Do not edit below this line
