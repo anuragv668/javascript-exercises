5e62b61876dc3037f88fa127d696ac29469ae138
anuragv668 <anuragvarma668@gmail.com>
2023-10-04 14:04:12 +0530

    complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  if (typeof num1 != 'number' || typeof num2 != 'number' || num1 < 0 || num2 < 0) {
+    return 'ERROR';
+  }
+  let first = num1;
+  let second = num2;
+  if (first > second) {
+    let temp = first;
+    first = second;
+    second = temp;
+  }
+  let sum = 0;
+  for (let i = first; i <= second; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
