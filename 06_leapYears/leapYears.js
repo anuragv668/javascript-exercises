aea28cc3685709d9d5c41de965f84c9992b0ccfd
anuragv668 <anuragvarma668@gmail.com>
2023-10-04 16:35:02 +0530

    complete 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  let isLeap = false;
 
+  if (year % 4 == 0) {
+    isLeap = true;
+    if (year % 100 == 0 && year % 400 != 0) {
+      isLeap = false;
+    }
+  }
+
+  return isLeap;
 };
 
 // Do not edit below this line
