d1725eb480d48b3184c323217b2c3955c37f3e14
anuragv668 <anuragvarma668@gmail.com>
2023-10-04 17:47:40 +0530

    complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(feh) {
+  let cel = (feh - 32) * 5 / 9;
+  return Math.round(cel * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  // return Math.round((cel * 5 / 9 + 32) * 10) / 10;
+  let feh = (cel * 9/5) + 32; 
+  return Math.round(feh * 10) / 10;
 };
 
 // Do not edit below this line
