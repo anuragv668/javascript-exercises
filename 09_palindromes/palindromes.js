9d90d0df13c769d3a338eab3a5e737840221d6e1
anuragv668 <anuragvarma668@gmail.com>
2023-10-06 11:51:56 +0530

    complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (arg) {
+  let lowered = arg.toLowerCase();
+  let punc = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+  let spc = /\s+/g; 
+  let string = lowered.replace(punc, '').replace(spc, '');
+  ;
 
+  let isPal;
+  for (let i = 0; i <= string.length / 2; i++ ) {
+    for (let j = string.length - 1 - i; j >= string.length / 2; j--) {
+      if (string[i] == string[j]) {
+        isPal = true;
+        break;
+      } else {
+        isPal = false;
+        break;
+      }
+    }
+  }
+
+
+  return isPal;
 };
 
 // Do not edit below this line
