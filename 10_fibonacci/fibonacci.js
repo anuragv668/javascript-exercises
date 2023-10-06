90578c23c37345332b3ea56be69a6ccc3ec2c4f8
anuragv668 <anuragvarma668@gmail.com>
2023-10-06 18:50:16 +0530

    complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let run = +num;
+  if (run < 0) {
+    return 'OOPS';
+  } 
+  let n1 = 1;
+  let n2 = 1;
+  let n3;
+  for (let i = 2; i < run; i++) {
+    n3 = n1 + n2;
+    n1 = n2;
+    n2 = n3;
+  }
+  return n2;
 };
 
 // Do not edit below this line
