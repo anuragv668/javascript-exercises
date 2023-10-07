3d3f516174aceff0097f05d353e65b41e6a5081a
anuragv668 <anuragvarma668@gmail.com>
2023-10-07 12:26:00 +0530

    complete 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+  let barr = [];
+  for (let i = 0; i < books.length; i++) {
+    barr.push(books[i]['title']);
+  }
+  return barr;
 };
 
 // Do not edit below this line
