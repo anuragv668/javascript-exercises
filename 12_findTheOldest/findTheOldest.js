768ad820a7853d300714648b20ef71334d75d6de
anuragv668 <anuragvarma668@gmail.com>
2023-10-08 11:02:06 +0530

    complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let ans;
+  let age = 0;
+  for (let i = 0;i < arr.length; i++) {
+    let d = new Date();
 
+    let c_age = d.getFullYear() - arr[i]['yearOfBirth'];
+
+    if (arr[i]['yearOfDeath']) {
+      c_age = arr[i]['yearOfDeath'] - arr[i]['yearOfBirth'];
+    }
+    if (c_age > age) {
+      age = c_age;
+      ans = arr[i];
+    }
+
+  }
+
+  return ans;
 };
 
 // Do not edit below this line
