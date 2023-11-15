b813cd0caa84a3dd6653a4d555c81ac1e4d38996
anuragv668 <anuragvarma668@gmail.com>
2023-11-15 19:57:42 +0530

    after array methods

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,25 +1,27 @@
 const palindromes = function (arg) {
-  let lowered = arg.toLowerCase();
-  let punc = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
-  let spc = /\s+/g; 
-  let string = lowered.replace(punc, '').replace(spc, '');
-  ;
-
-  let isPal;
-  for (let i = 0; i <= string.length / 2; i++ ) {
-    for (let j = string.length - 1 - i; j >= string.length / 2; j--) {
-      if (string[i] == string[j]) {
-        isPal = true;
-        break;
-      } else {
-        isPal = false;
-        break;
-      }
-    }
-  }
-
-
-  return isPal;
+  // let lowered = arg.toLowerCase();
+  // let punc = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g;
+  // let spc = /\s+/g; 
+  // let string = lowered.replace(punc, '').replace(spc, '');
+  // ;
+  //
+  // let isPal;
+  // for (let i = 0; i <= string.length / 2; i++ ) {
+  //   for (let j = string.length - 1 - i; j >= string.length / 2; j--) {
+  //     if (string[i] == string[j]) {
+  //       isPal = true;
+  //       break;
+  //     } else {
+  //       isPal = false;
+  //       break;
+  //     }
+  //   }
+  // }
+  //
+  //
+  // return isPal;
+  const filtered = arg.toLowerCase().replace(/[^a-z0-9]/g,'');
+  return filtered.split('').reverse().join('') == filtered;
 };
 
 // Do not edit below this line
