b0ecfebc3065c7ddda9562ef0112cbd0ac009fc5
anuragv668 <anuragvarma668@gmail.com>
2023-11-15 21:39:46 +0530

    return title using map method

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,11 @@
 const getTheTitles = function(books) {
-  let barr = [];
-  for (let i = 0; i < books.length; i++) {
-    barr.push(books[i]['title']);
-  }
-  return barr;
+  // let barr = [];
+  // for (let i = 0; i < books.length; i++) {
+  //   barr.push(books[i]['title']);
+  // }
+  // return barr;
+
+  return books.map((item) => item.title);
 };
 
 // Do not edit below this line
